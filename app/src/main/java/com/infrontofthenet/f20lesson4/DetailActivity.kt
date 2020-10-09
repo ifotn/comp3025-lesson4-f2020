2a63eaaec75f2b50e0225d73cb531ee3301b9de4
ifotn <rich@infrontofthenet.com>
2020-10-09 12:51:38 -0400

    l4 / implicit intent to load web page in browser

--- app/src/main/java/com/infrontofthenet/f20lesson4/DetailActivity.kt
+++ app/src/main/java/com/infrontofthenet/f20lesson4/DetailActivity.kt
@@ -1,11 +1,34 @@
 package com.infrontofthenet.f20lesson4
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        // read the name key from the extras object and display in our 2nd TextView
+        val name = intent.getStringExtra("name")
+        messageTextView.text = "Welcome $name"
+
+        browseButton.setOnClickListener {
+            // if we have a url entered, use an implicit intent to load that url in the browser
+            if (urlEditText.text.toString().isNotEmpty()) {
+                val url = urlEditText.text.toString()
+
+                // create a new intent and specify it's action is to View
+                val i = Intent(Intent.ACTION_VIEW)
+
+                // add data to the intent - the url we want to open
+                i.data = Uri.parse(url)
+
+                // launch the intent - startActivity() is used for both implicit & explicit intents
+                startActivity(i)
+            }
+        }
     }
 }
--- app/src/main/java/com/infrontofthenet/f20lesson4/MainActivity.kt
+++ app/src/main/java/com/infrontofthenet/f20lesson4/MainActivity.kt
@@ -3,6 +3,7 @@ package com.infrontofthenet.f20lesson4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         // event handler for Details button -> load Details activity using an explicit intent
         detailButton.setOnClickListener {
-            // create a new intent using our app's context, and the Detail activities java class object
-            val i = Intent(applicationContext, DetailActivity::class.java)
-            startActivity(i)
+            // only invoke intent if the Edit Text has a value entered
+            if (nameEditText.text.toString().isNotEmpty()) {
+                // create a new intent using our app's context, and the Detail activities java class object
+                val i = Intent(applicationContext, DetailActivity::class.java)
+
+                // use an extra to pass the text entered in the TextView to the Detail activity
+                i.putExtra("name", nameEditText.text.toString())
+
+                // launch the new activity using the intent
+                startActivity(i)
+            }
+            else {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
